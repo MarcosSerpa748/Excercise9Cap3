b22f4e16cb131ad75364538278c75f25f44ad994
MarcosSerpa748 <msrserpa@gmail.com>
2025-05-28 09:46:59 -0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,6 @@
-package br.com.principal;
-
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i<=10;i++){
-            GarbageColector gc = new GarbageColector();
-        }
-        System.out.println("Instâncias:"+ GarbageColector.getInstancias());
-        System.gc();
-        System.out.println("Instâncias:"+ GarbageColector.getInstancias());
-
+        Retangulo r1 = new Retangulo(1,2);
+        r1.calculoGeral(2);
     }
 }
--- src/Retangulo.java
+++ src/Retangulo.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class Retangulo {
+    private double base ;
+    private double altura;
+
+    Scanner sc = new Scanner(System.in);
+    public Retangulo(double base,double altura){
+        this.base = base;
+        this.altura = altura;
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void  calculoGeral(int numerosSalas){
+        for (int i = 0; i <= numerosSalas ; i++) {
+
+            System.out.println("Digite o valor da base:");
+            double base = sc.nextDouble();
+            System.out.println("Digite o valor da altura:");
+            double altura = sc.nextDouble();
+
+            Retangulo r1 = new Retangulo(base,altura);
+            double calculo = r1.getBase()* r1.getAltura();
+            System.out.println("Área:"+calculo);
+        }
+    }
 }
